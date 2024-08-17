af25884ef5fff767ebaa2bb39632135b7b040fd7
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-17 18:55:44 -0500

    Simplified test.

--- tests/manual/cmd.js
+++ tests/manual/cmd.js
@@ -2,7 +2,7 @@
 
 import {spawn} from 'dollar-shell';
 
-const cmd = spawn(['ls', '-l', '-a', 'src'], {stdout: 'inherit', stderr: 'inherit'});
+const cmd = spawn(['ls', '-l', '.'], {stdout: 'inherit', stderr: 'inherit'});
 
 await cmd.exited;
 console.log('DONE:', cmd.finished, cmd.killed, cmd.exitCode, cmd.signalCode)
