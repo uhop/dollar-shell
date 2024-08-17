b79ef7453f0064723fda8c243e56b0d6c55ba2e1
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-17 18:54:41 -0500

    Removed unused.

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-export const escapeArgument = arg => {
-  arg = String(arg);
-  const escaped = arg.replace("'", "'\\''");
-  return escaped !== arg ? "'" + escaped + "'" : arg;
-};
