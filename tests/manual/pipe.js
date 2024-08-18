fc11b0417da0befd68cbb7630c1953eac287f138
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-18 12:31:33 -0500

    Better test.

--- tests/manual/pipe.js
+++ tests/manual/pipe.js
@@ -2,12 +2,12 @@
 
 import {fromShell, toShell, sh} from 'dollar-shell';
 
-import process from 'node:process';
-import {Writable} from 'node:stream';
+// import process from 'node:process';
+// import {Writable} from 'node:stream';
 
 // Bun does not implement `Writable.toWeb()` yet
 // and it doesn't provide a simple way to write to `stdout` using web streams
 // fromShell`ls -l ${'.'}`.pipeThrough(sh`wc`).pipeTo(Writable.toWeb(process.stdout));
 // fromShell`ls -l ${'.'}`.pipeThrough(sh`wc`).pipeTo(new WritableStream(Bun.stdout.writer()));
 
-fromShell`ls -l ${'.'}`.pipeTo(toShell({stdout: 'inherit'})`wc`);
+fromShell`ls -l .`.pipeThrough(sh`grep LIC`).pipeTo(toShell({stdout: 'inherit'})`wc`);
