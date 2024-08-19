8f13bc3de73b35c90a8b7c03096cb73b1bba069e
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-18 23:16:43 -0500

    A test for `kill()`.

---
+++ tests/manual/inf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for ((;;)); do
+  echo Hello, world!
+done
---
+++ tests/manual/kill.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import {$$} from 'dollar-shell';
+
+const p = $$`./tests/manual/inf.sh`;
+
+console.log('Started...');
+await new Promise(resolve => setTimeout(resolve, 1000));
+p.kill();
+console.log('Killed...');
+await p.exited;
+console.log(p.killed, p.exitCode, p.signalCode);
