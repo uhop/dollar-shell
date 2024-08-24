b02dddf99b9f80884e525845e701316c8e7262fe
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-23 22:36:02 -0500

    Added examples for pipes.

--- tests/manual/shell.js
+++ tests/manual/shell.js
@@ -4,5 +4,8 @@ import {$sh} from 'dollar-shell';
 
 const result = await $sh({stdout: 'inherit'})`echo ${`hello'  \\wo"rld`}  ${'two   three'}`;
 // const result = await $sh({shellArgs: ['-ic'], stdout: 'inherit'})`nvm ls`;
+// const result = await $sh({stdout: 'inherit'})`ls -l . | grep LIC | wc`;
 
 console.log(result);
+
+// $sh.from`ls -l .`.pipeThrough($sh.io`grep LIC`).pipeTo($sh.to({stdout: 'inherit'})`wc`);
