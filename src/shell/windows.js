5fd0ad407833751e139afbc27ce6bd687c3aeeb8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-08-27 01:52:06 -0500

    Normalized before testing.

--- src/shell/windows.js
+++ src/shell/windows.js
@@ -7,7 +7,8 @@ export const shellEscape = s => String(s);
 export const currentShellPath = () => getEnv('ComSpec') || 'cmd.exe';
 
 export const buildShellCommand = (shell, args, command) => {
+  shell ||= currentShellPath();
   // derived from https://github.com/nodejs/node/blob/43f699d4d2799cfc17cbcad5770e1889075d5dbe/lib/child_process.js#L620
-  if (/^(?:.*\\)?cmd(?:\.exe)?$/i.test(shell)) return [shell || currentShellPath(), ...(args || ['/d', '/s', '/c']), '"' + command + '"'];
-  return [shell || currentShellPath(), ...(args || ['-c']), command];
+  if (/^(?:.*\\)?cmd(?:\.exe)?$/i.test(shell)) return [shell, ...(args || ['/d', '/s', '/c']), '"' + command + '"'];
+  return [shell, ...(args || ['-c']), command];
 };
