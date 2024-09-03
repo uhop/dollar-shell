8d676d877085fb25cebaabc52c573491da6c7bf8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-09-02 23:16:17 -0500

    More accurate work with PowerShell.

--- src/shell/windows.js
+++ src/shell/windows.js
@@ -10,7 +10,6 @@ const isPwsh = s => /^(?:.*\\)?(?:pwsh|powershell)(?:\.exe)?$/i.test(s);
 export const shellEscape = (s, options) => {
   s = String(s);
   const shell = options?.shell || currentShellPath();
-  // return s.replace(/[\\'"\^%]|(?:\r?\n)/g, '^$&');
   if (isCmd(shell)) return s.replace(/[\\'"\^%]|(?:\r?\n)/g, '^$&');
   // if (isPwsh(shell)) return '"' + s.replace(/"/g, "`\"") + '"';
   return s;
@@ -20,6 +19,10 @@ export const buildShellCommand = (shell, args, command) => {
   shell ||= currentShellPath();
   // derived from https://github.com/nodejs/node/blob/43f699d4d2799cfc17cbcad5770e1889075d5dbe/lib/child_process.js#L620
   if (isCmd(shell)) return [shell, ...(args || ['/d', '/s', '/c']), command];
-  if (isPwsh(shell)) return [shell, ...(args || ['-e']), toBase64(command)];
+  if (isPwsh(shell)) {
+    args ||= ['-e'];
+    if (args.includes('-e')) command = toBase64(command);
+    return [shell, ...args, command];
+  }
   return [shell, ...(args || ['-c']), command];
 };
